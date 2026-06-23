fd9477a11c226b67bf2b1e043bdac8472374c321
0hneB <192783337+0hneB@users.noreply.github.com>
2026-06-23 20:00:48 +0200

    chore: add serve.bat dev launcher and ignore assets/

---
+++ serve.bat
@@ -0,0 +1,8 @@
+@echo off
+rem Double-click this to play locally. It serves the folder over http://
+rem (browsers block ES modules + fetch over file://), then opens the browser.
+cd /d "%~dp0"
+echo Serving FreeGuessr at http://localhost:8000
+echo Keep this window open while playing. Close it to stop the server.
+start "" http://localhost:8000
+python -m http.server 8000
