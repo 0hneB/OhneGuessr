fa427a93ee0f7e4cc461518fdc57c527995aefbf
0hneB <192783337+0hneB@users.noreply.github.com>
2026-09-11 11:31:38 +0200

    feat(ui): add Dark Mode theme

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -5,6 +5,7 @@ export type GuessMapSize = 'default' | 'large' | 'xl' | 'xxl';
 export type CompassStyle = 'bar' | 'classic' | 'both';
 export type LauncherTheme =
   | 'ohneguessr'
+  | 'dark-mode'
   | 'ohneb'
   | 'gruvbox-dark-soft'
   | 'gruvbox-light-soft'
